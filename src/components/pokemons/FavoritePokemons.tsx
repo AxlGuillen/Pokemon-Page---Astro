b00121745085ec4d8d226163fd7e7dd7aa3e40fe
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-11-05 22:51:42 -0600

    Favorite pokemons card creada y implementada

---
+++ src/components/pokemons/FavoritePokemons.tsx
@@ -0,0 +1,27 @@
+import type { FavoritePokemon } from "@interfaces/favorite-pokemon";
+import { For, createSignal } from "solid-js";
+import { FavoritePokemonCard } from "./FavoritePokemonCard";
+
+const getLocalStoragePokemos = (): FavoritePokemon[] => {
+    const favoritePokemons = JSON.parse(localStorage.getItem('favorites') ?? '[]');
+
+    return favoritePokemons;
+}
+
+export const FavoritePokemos = () => {
+
+    const [ pokemons, setPokemons ] = createSignal(getLocalStoragePokemos());
+
+
+    return (
+        <div class="grid grid-cols-2 sm:grid-cols-4">
+            <For each={pokemons()}>
+                {
+                    (pokemon) => (
+                        <FavoritePokemonCard pokemon={pokemon} />
+                    )
+                }
+            </For>
+        </div>
+    )
+}
